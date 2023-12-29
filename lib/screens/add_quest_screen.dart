15fc2d23e3a7c7bf884c6b96dd9fa9681db70d9c
TitanFox13 <honzik@juniorskicamp.cz>
2023-12-29 17:52:12 +0100

    commint

--- lib/screens/add_quest_screen.dart
+++ lib/screens/add_quest_screen.dart
@@ -33,6 +33,7 @@ class _AddQuestScreenState extends State<AddQuestScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text(_gameCode),
       ),
--- lib/screens/create_game_screen.dart
+++ lib/screens/create_game_screen.dart
@@ -21,102 +21,105 @@ class CreateGameScreen extends ConsumerWidget {
         gameInfo.quests.isNotEmpty ? gameInfo.quests : ['No quests selected'];
     String title = ref.read(currentGameProvider.notifier).getTitle();
     return Scaffold(
+        resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 15,
-            child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: FittedBox(
-                fit: BoxFit.fitHeight,
-                child: Text(
-                  'Quests:',
-                  style: TextStyle(color: Colors.deepOrange),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 15,
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: FittedBox(
+                  fit: BoxFit.fitHeight,
+                  child: Text(
+                    'Quests:',
+                    style: TextStyle(color: Colors.deepOrange),
+                  ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 2.5,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20 , 0, 20, 20),
-              child: Material(
-                child: ListView.builder(
-                  itemCount: quests.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      title: Text(quests[index]!),
-                    );
-                  },
-                  shrinkWrap: false,
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 4,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20 , 0, 20, 20),
+                child: Material(
+                  child: ListView.builder(
+                    itemCount: quests.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text(quests[index]!),
+                      );
+                    },
+                    shrinkWrap: false,
+                  ),
                 ),
               ),
             ),
-          ),
-          MyElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const BrowseQuestsScreen()),
-                );
-              },
-              text: 'Browse quests'),
-          MyElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => AddQuestScreen(gameCode: title)),
-                );
-              },
-              text: 'Add quests'),
-          Increment(
-              text: 'Number of impostors',
-              initialValue: 1,
-              onPlus: () async {
-                await ref
-                    .read(currentGameProvider.notifier)
-                    .increaseImpostorCount();
-              },
-              onMinus: () async {
-                await ref
-                    .read(currentGameProvider.notifier)
-                    .decreaseImpostorCount();
-              }),
-          Increment(
-              text: 'Number of quests',
-              initialValue: 1,
-              onPlus: () async {
-                await ref
-                    .read(currentGameProvider.notifier)
-                    .increaseNumberOfQuests();
-              },
-              onMinus: () async {
-                await ref
-                    .read(currentGameProvider.notifier)
-                    .decreaseNumberOfQuests();
-              }),
-          MyElevatedButton(
-              onPressed: () async {
-                await ref.read(currentGameProvider.notifier).startGame();
-                String name =
-                    ref.read(currentGameProvider.notifier).getPlayerName();
-                List impostors =
-                    await ref.read(currentGameProvider.notifier).getImpostors();
-                if (impostors.contains(name)) {
-                  Navigator.of(context).pushNamed(ImpostorScreen.routeName);
-                } else {
-                  Navigator.of(context).pushNamed(CrewmateScreen.routeName);
-                }
-              },
-              text: 'Start game'),
-        ],
+            MyElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BrowseQuestsScreen()),
+                  );
+                },
+                text: 'Browse quests'),
+            MyElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AddQuestScreen(gameCode: title)),
+                  );
+                },
+                text: 'Add quests'),
+            Increment(
+                text: 'Number of impostors',
+                initialValue: 1,
+                onPlus: () async {
+                  await ref
+                      .read(currentGameProvider.notifier)
+                      .increaseImpostorCount();
+                },
+                onMinus: () async {
+                  await ref
+                      .read(currentGameProvider.notifier)
+                      .decreaseImpostorCount();
+                }),
+            Increment(
+                text: 'Number of quests',
+                initialValue: 1,
+                onPlus: () async {
+                  await ref
+                      .read(currentGameProvider.notifier)
+                      .increaseNumberOfQuests();
+                },
+                onMinus: () async {
+                  await ref
+                      .read(currentGameProvider.notifier)
+                      .decreaseNumberOfQuests();
+                }),
+            MyElevatedButton(
+                onPressed: () async {
+                  await ref.read(currentGameProvider.notifier).startGame();
+                  String name =
+                      ref.read(currentGameProvider.notifier).getPlayerName();
+                  List impostors =
+                      await ref.read(currentGameProvider.notifier).getImpostors();
+                  if (impostors.contains(name)) {
+                    Navigator.of(context).pushNamed(ImpostorScreen.routeName);
+                  } else {
+                    Navigator.of(context).pushNamed(CrewmateScreen.routeName);
+                  }
+                },
+                text: 'Start game'),
+          ],
+        ),
       ),
     );
   }
